313f06574f775ba4621f6656b880083b8b680de7
Jaishree <dgjai27@gmail.com>
2025-08-19 23:41:41 +0530

    Simple Sieve algorithm implementation

---
+++ src/SimpleSieve.java
@@ -0,0 +1,24 @@
+public class SimpleSieve{
+    public static void sieve(int n){
+        boolean[] prime = new boolean[n+1];
+        for(int i=2; i<=n; i++){
+            prime[i] = true;
+        }
+        for(int p=2; p*p <= n; p++){
+            if(prime[p] == true){
+                for(int i=p*p; i<=n; i+=p){
+                    prime[i] = false;
+                }
+            }
+        }
+        for(int i=2; i<=n; i++){
+            if(prime[i] == true){
+                System.out.print(i+" ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        sieve(50);
+    }
+}
