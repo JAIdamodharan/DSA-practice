7a70b6b930d08e1d9a173ee98c641688ae0dfbdb
Jaishree <dgjai27@gmail.com>
2025-08-20 00:54:59 +0530

    strobogrammatic no  algo

---
+++ src/StroboClass.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class StroboClass {
+    public static boolean strobo(String num) {
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        map.put('6', '9');
+        map.put('9', '6');
+        map.put('0', '0');
+        map.put('1', '1');
+        map.put('8', '8');
+        int l = 0, r = num.length() - 1;
+        while (l <= r) {
+            if (!map.containsKey(num.charAt(l))) {
+                return false;
+            }
+            if (map.get(num.charAt(l)) != num.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strobo("6119"));
+    }
+}
