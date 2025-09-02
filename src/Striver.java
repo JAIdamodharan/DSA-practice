86b78316bf1263a962e95f6b326fd4a51d692b2a
Jaishree <dgjai27@gmail.com>
2025-09-02 22:29:17 +0530

    striver playlist practice

--- src/Striver.java
+++ src/Striver.java
@@ -10,6 +10,15 @@ public class Striver {
 //            System.out.println();
 //        }
 //    }
+    //For Triangle pattern
+    public static void printPattern(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
 //        Scanner o1 = new Scanner(System.in);
 //        int t = o1.nextInt();
@@ -17,5 +26,10 @@ public class Striver {
 //            int n = o1.nextInt();
 //            printPattern(n);
 //        }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printPattern(n);
+
+
     }
 }
