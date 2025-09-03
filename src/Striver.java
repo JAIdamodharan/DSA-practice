76e7da2cf2068f23c77a4fa8ce01b67eb75c3a83
Jaishree <dgjai27@gmail.com>
2025-09-03 22:48:07 +0530

    next pattern - triangle with numbers - incremenatal order

--- src/Striver.java
+++ src/Striver.java
@@ -34,7 +34,7 @@ public class Striver {
         int n = sc.nextInt();
         for(int i=1 ; i<=n; i++){
             for(int j=1 ; j<=i ; j++){
-                System.out.print(i);
+                System.out.print(j);
             }
             System.out.println();
         }
