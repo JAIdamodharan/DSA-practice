4cd94f5d7c0c5ae7c01468ef2a6e064fc227eabb
Jaishree <dgjai27@gmail.com>
2025-09-03 22:42:22 +0530

    next pattern - triangle with numbers

--- src/Striver.java
+++ src/Striver.java
@@ -33,7 +33,7 @@ public class Striver {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=1 ; i<=n; i++){
-            for(int j=1 ; j<=n ; j++){
+            for(int j=1 ; j<=i ; j++){
                 System.out.print(i);
             }
             System.out.println();
