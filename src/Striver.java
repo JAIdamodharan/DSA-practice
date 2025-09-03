2dc1e75442368020ad5ec361f7f641dba3eaa789
Jaishree <dgjai27@gmail.com>
2025-09-03 22:41:07 +0530

    next pattern

--- src/Striver.java
+++ src/Striver.java
@@ -30,5 +30,13 @@ public class Striver {
 //        Scanner sc = new Scanner(System.in);
 //        int n = sc.nextInt();
 //        printPattern(n);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1 ; i<=n; i++){
+            for(int j=1 ; j<=n ; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 }
