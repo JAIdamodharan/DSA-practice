e9d5471ae0934aa8c1601025ae77278a0cefbdeb
Jaishree <dgjai27@gmail.com>
2025-10-07 21:28:45 +0530

    next pattern - triangle with numbers - incremental order

--- src/Striver.java
+++ src/Striver.java
@@ -30,11 +30,15 @@ public class Striver {
 //        Scanner sc = new Scanner(System.in);
 //        int n = sc.nextInt();
 //        printPattern(n);
+//****
+//***
+//**
+//*
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=1 ; i<=n; i++){
-            for(int j=1 ; j<=i ; j++){
-                System.out.print(j);
+            for(int j=0 ; j<=n-i ; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
--- src/Toggle.java
+++ src/Toggle.java
@@ -2,21 +2,37 @@ import java.util.*;
 
 public class Toggle {
     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        boolean[] b = new boolean[n+1];
+//        int c=0, o=0;
+//        for(int i=1; i<=n; i++){
+//            for(int j=i; j<=n; j+=i){
+//                b[j] = !b[j];
+//            }
+//        }
+//        for(int a=0; a<n; a++){
+//            if(b[a]){
+//                o++;
+//            } else c++;
+//        }
+//        System.out.println(o);
+//        System.out.println(c);
+        //Proper Toggle code
+
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        boolean[] b = new boolean[n+1];
-        int c=0, o=0;
-        for(int i=1; i<=n; i++){
-            for(int j=i; j<=n; j+=i){
-                b[j] = !b[j];
+        String s = sc.nextLine();
+        StringBuilder result = new StringBuilder();
+        for(int i=0 ; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if (ch >= 'A' && ch <= 'Z'){
+                result.append((char)(ch+32));
+            } else if (ch >= 'a' && ch <= 'z'){
+                result.append((char)(ch-32));
+            } else{
+                result.append(ch);
             }
         }
-        for(int a=0; a<n; a++){
-            if(b[a]){
-                o++;
-            } else c++;
-        }
-        System.out.println(o);
-        System.out.println(c);
+        System.out.println(result.toString());
     }
 }
