cb3c97274bd7c6261ab4957a59cd9931461509ee
Jaishree <dgjai27@gmail.com>
2026-02-04 14:41:53 +0530

    next module

--- src/MaxSlidingWindow.java
+++ src/MaxSlidingWindow.java
@@ -1,43 +1,24 @@
 import java.util.*;
 
 public class MaxSlidingWindow {
-
-    public static int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums.length == 0 || k == 0)
-            return new int[0];
+    public static int[] maxSW(int[] nums, int k){
+        if(nums.length == 0) return new int[0];
 
         int n = nums.length;
-        int[] result = new int[n - k + 1];
-        Deque<Integer> dq = new ArrayDeque<>();
-
-        for (int i = 0; i < n; i++) {
-
-            // Remove indices out of current window
-            while (!dq.isEmpty() && dq.peekFirst() <= i - k)
-                dq.pollFirst();
-
-            // Remove smaller elements
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i])
+        int[] res = new int[n - k + 1];
+        Deque<Integer> dq = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            if(!dq.isEmpty() && dq.peek() < i-k+1){
+                dq.poll();
+            }
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
                 dq.pollLast();
-
-            dq.offerLast(i);
-
-            // Window ready
-            if (i >= k - 1)
-                result[i - k + 1] = nums[dq.peekFirst()];
+            }
+            dq.offer(i);
+            if(i >= k - 1){
+                res[i - k + 1] = nums[dq.peek()];
+            }
         }
-
-        return result;
-    }
-
-    // MAIN FUNCTION
-    public static void main(String[] args) {
-        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
-        int k = 3;
-
-        int[] ans = maxSlidingWindow(nums, k);
-
-        System.out.println("Maximum of each sliding window:");
-        System.out.println(Arrays.toString(ans));
+        return res;
     }
 }
--- src/SegregateEvenOdd.java
+++ src/SegregateEvenOdd.java
@@ -1,65 +1,59 @@
-class OddEvenLinkedList {
-
-    // Definition for singly-linked list.
-    static class ListNode {
+public class SegregateEvenOdd{
+    public static class ListNode{
         int val;
         ListNode next;
 
-        ListNode(int val) {
+        public ListNode(int val){
             this.val = val;
             this.next = null;
         }
     }
 
-    // Function to rearrange odd and even indexed nodes
-    public static ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null)
+    public static ListNode oddEvenList(ListNode head){
+        if(head == null || head.next == null){
             return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
 
-        ListNode odd = head;          // 1st node
-        ListNode even = head.next;    // 2nd node
-        ListNode evenHead = even;     // store start of even list
-
-        while (even != null && even.next != null) {
-            odd.next = even.next;     // link odd nodes
+        while(even != null && even.next != null){
+            odd.next = even.next;
             odd = odd.next;
 
-            even.next = odd.next;     // link even nodes
+            even.next = odd.next;
             even = even.next;
         }
 
-        odd.next = evenHead;          // attach even list after odd list
+        odd.next = evenHead;
         return head;
     }
 
-    // Utility function to print the linked list
-    public static void printList(ListNode head) {
+    public static void printList(ListNode head){
         ListNode temp = head;
-        while (temp != null) {
+        while(temp != null){
             System.out.print(temp.val);
-            if (temp.next != null)
+            if(temp.next != null){
                 System.out.print(" -> ");
+            }
             temp = temp.next;
         }
         System.out.println();
     }
 
-    // Main function
     public static void main(String[] args) {
-
-        // Example 1: [1,2,3,4,5]
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
 
-        System.out.print("Original List: ");
+        System.out.println("Original List: ");
         printList(head);
 
         head = oddEvenList(head);
 
-        System.out.print("Odd-Even List: ");
+        System.out.println("Odd-even List: ");
         printList(head);
     }
 }
